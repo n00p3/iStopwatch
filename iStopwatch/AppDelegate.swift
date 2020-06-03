b0a18d0d2ebfcf4bf8831436c136a70209e2a891
Mateusz Głowski <mateusz.glowski@daw-systems.pl>
2020-06-03 15:27:32 +0200

    First commit

--- iStopwatch/AppDelegate.swift
+++ iStopwatch/AppDelegate.swift
@@ -11,16 +11,60 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    var statusItem: NSStatusItem!
+    var time = 0.0
+    var isRunning = false
+    var timer: Timer?
+    
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        statusItem.button?.image = NSImage(named: NSImage.Name("baseline_timer_black_18dp"))
+        statusItem.button?.imagePosition = .imageLeft
+        statusItem.button?.sendAction(on: [.leftMouseUp, .rightMouseUp])
+        statusItem.button?.action = #selector(itemClicked(_:))
+        statusItem.button?.title = ""
+    
+        self.statusItem = statusItem
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
+    
+    @objc func itemClicked(_ sender: Any?) {
+        if NSApp.currentEvent!.type == .rightMouseUp {
+            print("right")
+            self.statusItem.button?.title = ""
+            timer?.invalidate()
+            self.time = 0.0
+            self.isRunning = false
+            return
+        } else if NSApp.currentEvent!.type == .leftMouseUp {
+            print("left")
+        } else {
+            print("Unknown key clicked.")
+        }
+        
+        isRunning = !isRunning
 
-
+        statusItem.button?.title = secondsToHoursMinutesSeconds(seconds: time)
+        
+        timer?.invalidate()
+        time = 0.0
+        if isRunning {
+            timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { (timer) in
+                self.time += timer.timeInterval
+                self.statusItem.button?.title = self.secondsToHoursMinutesSeconds(seconds: self.time)
+            }
+        }
+    }
+    
+    func secondsToHoursMinutesSeconds(seconds : Double) -> String {
+      let (hr,  minf) = modf (seconds / 3600)
+      let (min, secf) = modf (60 * minf)
+      return "\(String(format: "%02d", hr)):\(String(format: "%02d", min)):\(String(format: "%04.1f", 60 * secf))"
+    }
 }
 
