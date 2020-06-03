33b0ca00dec6105446a810f9a733e23739ef6558
Mateusz Głowski <mateusz.glowski@daw-systems.pl>
2020-06-03 13:53:41 +0200

    Initial Commit

---
+++ iStopwatch/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iStopwatch
+//
+//  Created by Mateusz Głowski on 03/06/2020.
+//  Copyright © 2020 Mateusz Głowski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iStopwatch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iStopwatch
+//
+//  Created by Mateusz Głowski on 03/06/2020.
+//  Copyright © 2020 Mateusz Głowski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
